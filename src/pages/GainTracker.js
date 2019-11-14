57ac6a7e5904a91cca86748f133bfcdf9db78552
Cristian Caratti <ccristian.caratti.xx@gmail.com>
2019-11-14 23:08:48 +0000

    created new component InvestementResult to  calculate and display results from API call

--- src/pages/GainTracker.js
+++ src/pages/GainTracker.js
@@ -12,13 +12,14 @@ import MomentLocaleUtils, {
 } from "react-day-picker/moment";
 
 import IntroGainTracker from "../components/IntroGainTracker";
-import CurrentPrices from "../components/CurrentPrices";
+import InvestmentResult from "../components/InvestmentResult";
 
 export default class GainTracker extends Component {
   state = {
     currentBTC: "",
     historicBTC: {},
-    selectedDay: undefined
+    selectedDay: undefined,
+    cryptoAmount: 1
   };
 
   getPriceHistoricData = async () => {
@@ -32,7 +33,7 @@ export default class GainTracker extends Component {
   };
 
   getCurrentPrice = async () => {
-    const link = `/pricemulti?fsyms=BTC,ETH,DFASH,LTC&tsyms=BTC,ETH,DASH,USD,EUR,GBP&api_key=${process.env.REACT_APP_API_URL}`;
+    const link = `/pricemulti?fsyms=BTC,ETH,DASH,LTC&tsyms=USD,EUR,GBP&api_key=${process.env.REACT_APP_API_URL}`;
     const response = await cryptoCompare.get(link);
     this.setState({
       currentBTC: response.data.BTC
@@ -55,56 +56,82 @@ export default class GainTracker extends Component {
     });
   };
 
-  onSubmit = e => {
-    e.prevenrDefault();
+  onClick = () => {
+    // CP =  Current Price
+    // HP =  Storic Price
+    const HP = this.state.historicBTC.GBP;
+    const CP = this.state.currentBTC.GBP;
+    // newCP = CP that consider the q.ty bought
+    let newCP = this.state.cryptoAmount * 100;
+    newCP = (newCP * CP) / 100;
+    // newHP = HP that consider the q.ty bought
+    let newHP = this.state.cryptoAmount * 100;
+    newHP = (newHP * HP) / 100;
+    if (newCP > newHP) {
+      const gain = newCP - newHP;
+      const gainPercent = ((gain / newCP) * 100).toFixed(2);
+      console.log(`The profit is ${gainPercent} that is equal to £${gain}`);
+    } else {
+      const loss = newHP - newCP;
+      const lossPercent = ((loss / newHP) * 100).toFixed(2);
+      console.log(`The loss is: ${lossPercent} that is  £${loss}`);
+    }
+  };
+
+  onInputChange = e => {
+    this.setState({
+      cryptoAmount: e.target.value
+    });
   };
 
   render() {
     console.log(this.state.currentBTC);
-
     console.log(this.state.historicBTC);
-    const date = this.state.historicBTC;
 
     const { selectedDay } = this.state;
     // console.log(moment(this.state.selectedDay).unix());
 
+    console.log(`This is the amount: ${this.state.cryptoAmount}`);
+    console.log(this.props.gain);
     return (
       <div>
         <IntroGainTracker />
-        {/* <CurrentPrices prices={this.state.currentBTC} /> */}
         <TransactionContainer>
           <h2>Enter Transaction</h2>
 
           <Form>
-            <form action="" onFormSubmit={this.onSubmit}>
-              <label htmlFor="">Amount</label>
-              <input type="text" name="amount" placeholder="ex. £ 1,000.00" />
-              <label htmlFor="">Date</label>
-              <DayPickerInput
-                formatDate={formatDate}
-                parseDate={parseDate}
-                placeholder={`${formatDate(new Date())}`}
-                value={selectedDay}
-                onDayChange={this.handleDayChange}
-                dayPickerProps={{
-                  selectedDays: selectedDay,
-                  localeUtils: MomentLocaleUtils
-                }}
-              />
-              <button type="submit">Check Profits</button>
-            </form>
+            <label htmlFor="">Amount</label>
+            <input
+              type="text"
+              name="amount"
+              value={this.state.cryptoAmount}
+              onChange={this.onInputChange}
+              placeholder="ex. £ 1,000.00"
+            />
+            <label htmlFor="">Date</label>
+            <DayPickerInput
+              formatDate={formatDate}
+              parseDate={parseDate}
+              placeholder={`${formatDate(new Date())}`}
+              value={selectedDay}
+              onDayChange={this.handleDayChange}
+              dayPickerProps={{
+                selectedDays: selectedDay,
+                localeUtils: MomentLocaleUtils
+              }}
+            />
+            <button type="submit" onClick={this.onClick}>
+              Check Profits
+            </button>
           </Form>
-          <p>You bought on {this.state.date} xxx Bitcoins:</p>
         </TransactionContainer>
-        Results here:
         <br />
-        {date.GBP}
-        <br />
-        {/* {date.map(data => (
-          <DayPurchase key={data.time}>
-            <span>{moment.unix(data.time).format("LLL")}</span>
-          </DayPurchase>
-        ))} */}
+        <InvestmentResult
+          currentPrice={this.state.currentBTC}
+          currentQty={this.state.cryptoAmount}
+          historicPrice={this.state.historicBTC.GBP}
+          gain={this.props.gain}
+        />
       </div>
     );
   }
@@ -127,9 +154,4 @@ const Form = styled.div`
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
-`;
-
-const DayPurchase = styled.div`
-  display: flex;
-  flex-flow: column wrap;
 `;
